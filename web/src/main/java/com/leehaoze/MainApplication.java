12dfd03182c4f32e4c9fdca12650251ea191bf47
leehaoze <leehaoze@outlook.com>
2020-01-07 20:29:51 +0800

    添加程序启动入口

---
+++ web/src/main/java/com/leehaoze/MainApplication.java
@@ -0,0 +1,14 @@
+package com.leehaoze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author leehaoze
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
